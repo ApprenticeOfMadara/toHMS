40f6a870b51bc49a36bfd3d52909a4c530784197
ApprenticeOfMadara <87611306+ApprenticeOfMadara@users.noreply.github.com>
2021-07-24 13:47:55 +0100

    Update fromSeconds.lua

--- fromSeconds.lua
+++ fromSeconds.lua
@@ -1,12 +1,12 @@
 local function toHMS(s, twelveHourFormat) -- Takes seconds as first param and bool as second
 	if not twelveHourFormat then
 		return string.format("%02i:%02i:%02i", s/3600, s/60%60, s%60)
-  end
-	
+	end
+
 	local hours = s/3600
 	local over12 = hours > 12
-		
+
 	hours = (over12 and hours - 12) or hours
-		
+
 	return string.format("%02i:%02i:%02i", hours, s/60%60, s%60) .. (over12 and " PM" or " AM")
 end
